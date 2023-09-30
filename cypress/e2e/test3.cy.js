8aa10efca440f021b10daaaec9d4877b779a5bb9
Rasha Al-zaben <alzabenr13@gmail.com>
2023-09-30 04:36:08 +0300

    update 2

--- cypress/e2e/test3.cy.js
+++ cypress/e2e/test3.cy.js
@@ -1,5 +1,14 @@
+///<reference types="cypress">
+cy.on("uncaught:exception",(e)=>{
+  return false;
+});
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    let websites=["https://sa.almosafer.com/en","https://sa.almosafer.com/ar"]
+
+    let RandomIndex=Math.floor(Math.random()*websites.length)
+    cy.visit(websites[RandomIndex])
+    cy.get('.cta__saudi').click()
+
+  });
+});
