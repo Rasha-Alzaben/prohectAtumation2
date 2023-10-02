cfb56f4604e2aa39ba45798389abac911b64816f
Rasha Al-zaben <alzabenr13@gmail.com>
2023-10-02 15:55:23 +0300

    update lang

--- cypress/e2e/test3.cy.js
+++ cypress/e2e/test3.cy.js
@@ -1,7 +1,7 @@
 ///<reference types="cypress">
-cy.on("uncaught:exception",(e)=>{
+/*cy.on("uncaught:exception",(e)=>{
   return false;
-});
+});*/
 describe('template spec', () => {
   it('passes', () => {
     let websites=["https://sa.almosafer.com/en","https://sa.almosafer.com/ar"]
@@ -9,6 +9,15 @@ describe('template spec', () => {
     let RandomIndex=Math.floor(Math.random()*websites.length)
     cy.visit(websites[RandomIndex])
     cy.get('.cta__saudi').click()
+    cy.get('#uncontrolled-tab-example-tab-hotels > .sc-dWcDbm').click()
+
+    if(RandomIndex==0){
+      cy.get('[data-testid="AutoCompleteInput"]').type("dubai")
+    }
+    else if(RandomIndex==1){
+      cy.get('[data-testid="AutoCompleteInput"]').type('دبي')
+
+    }
 
   });
 });
